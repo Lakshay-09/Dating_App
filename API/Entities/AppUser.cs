1fd382fa859f0506a705762ea4fa32abdea1ff35
Lakshay-09 <lakshaybainp@gmail.com>
2024-01-12 13:30:52 +0530

    Section2end

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+namespace API.Entities;
+
+// Id will be default primary key in entity framework 
+public class AppUser 
+{
+    
+    public int Id { get; set; }
+    public string UserName { get; set;}
+}
